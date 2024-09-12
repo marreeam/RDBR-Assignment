614a73daf021171b51f46674dc0406c32be730eb
მარიამ მარგალიტაძე <mariamargalitadze9@gmail.com>
2024-09-12 13:52:13 +0400

    Add Header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-[1920px] h-[100px] p-[38px_162px] flex items-center gap-[10px] border border-[#DBDBDB] bg-[#FFFFFF]">
+       <img   src="/images/logo.png"  
+        alt="logo"/>
+   
+    </header>
+  );
+};
+
+export default Header;
